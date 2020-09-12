9de964cdc06be5a37736e501561a6f3fb5c2dd2f
PauloAlves8039 <pj38alves@gmail.com>
2020-09-12 14:30:02 -0300

    Versão inicial do projeto

---
+++ e2e/src/app.e2e-spec.ts
@@ -0,0 +1,16 @@
+import { AppPage } from './app.po';
+import { browser, logging } from 'protractor';
+
+describe('workspace-project App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getTitleText()).toEqual('gerenciador-de-tarefas app is running!');
+  });
+
+});
